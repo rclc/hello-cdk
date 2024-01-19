b4936aee07888c5a416435a2857c672676a32a9a
EC2 Default User <ec2-user@ip-172-31-21-145.ec2.internal>
2024-01-19 23:09:01 +0000

    First commit

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,19 +1,21 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
-from constructs import Construct
+import aws_cdk as cdk
+import aws_cdk.aws_s3 as s3
+import aws_cdk.aws_s3_deployment as s3deployment
+            
+class HelloCdkStack(cdk.Stack):
 
-class HelloCdkStack(Stack):
-
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "HelloCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bucket = s3.Bucket(self, "MyFirstBucket",
+                          block_public_access=s3.BlockPublicAccess.BLOCK_ACLS,
+                          access_control=s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+                          public_read_access=True,
+                          website_index_document="index.html",
+                          versioned=True, 
+                          removal_policy=cdk.RemovalPolicy.DESTROY, 
+                          auto_delete_objects=True)
+        
+        deployment = s3deployment.BucketDeployment(self, "DeployWebsite", 
+                            sources=[s3deployment.Source.asset('./hello_cdk/static-website.zip')],
+                            destination_bucket=bucket)
